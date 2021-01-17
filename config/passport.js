91f87372a92d34ae6249d12fde07691b7ab11589
simran-bhadani3 <simranbhadani333@gmail.com>
2021-01-17 12:19:55 +0800

    passport configuration

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('./keys');
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys.jwtKey    
+};
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        User.findById(jwt_payload.id)
+        .then(user => {
+            if(user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+        .catch(err => console.log(err));
+    }));
+}
