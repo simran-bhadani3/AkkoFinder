95896e7df392bdfa143c0d38c8967171f0317f73
simran-bhadani3 <simranbhadani333@gmail.com>
2021-02-22 13:25:59 +0800

    Set up for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./connection");
 const passport = require("passport");
+const path = require("path");
 
 const user = require("./routes/api/user");
 const accomodation = require("./routes/api/accomodation");
@@ -15,14 +16,19 @@ app.use(express.urlencoded({ extended: true }));
 //connect to database
 db();
 
-app.use(passport.initialize());
-
 require("./config/passport.js")(passport);
 
 //routes
 app.use("/api/user", user);
 app.use("/api/accomodation", accomodation);
 app.use("/api/accomodationdata", accomodationdata);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
 
 const port = process.env.PORT || 5000;
 
